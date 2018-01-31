a4162913f669882d1256a3c47140c948b6d91e83
NoahOrberg <noah.orberg.9999@gmail.com>
2018-01-31 21:51:50 +0900

    modify endpoint

--- src/app.rb
+++ src/app.rb
@@ -2,7 +2,10 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/json'
 require 'json'
-require_relative 'dba.rb'
+
+require_relative 'dba/type.rb'
+require_relative 'dba/market.rb'
+require_relative 'dba/spending_history.rb'
 
 class MainApp < Sinatra::Base
     configure :development do
@@ -12,9 +15,13 @@ class MainApp < Sinatra::Base
     # これより上は気にしなくていい
 
     def initialize
+        # スーパークラスのコンストラクタ呼び出し
         super()
+
         # DBへアクセスするためのクラスを初期化し、フィールドに格納
-        @useraccess = UserAccess.new
+        @type_access = TypeAccess.new
+        @market_access = MarketAccess.new
+        @spending_access = SpendingHistoryAccess.new
     end
 
     get '/' do
@@ -25,13 +32,25 @@ class MainApp < Sinatra::Base
         'pong'
     end
 
-    post '/users', provides: :json do
+    # 店情報を追加するエンドポイント
+    # このエンドポイントのjsonのparameterは、
+    #   {
+    #        "name" : String,
+    #   }
+    post '/markets', provides: :json do
+        # HTTPリクエストのJSONのparameterをRubyで扱えるようにパースする
+        # :keyがキーになる (e.g, params[:name])
         params = JSON.parse(request.body.read, {:symbolize_name => true})
-        @useraccess.add_user(params)
-        status 200
+
+        @market_access.add_market(params)
+        status 200 # 成功
     end
 
+    # 店情報を取得するエンドポイント
     get '/users' do
-        json (@useraccess.user_all)
+        # getはクライアントが情報の塊であるjsonを取得したいので、`json (XXXX)` で返す
+        json (@market_access.all)
     end
+
+    # 問題に応じて追加していく
 end
