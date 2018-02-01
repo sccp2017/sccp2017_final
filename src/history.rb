252d6f315ae590fa8c9a9d2bab99a90c6707ab83
NoahOrberg <noah.orberg.9999@gmail.com>
2018-02-01 14:08:12 +0900

    fix controller

--- src/history.rb
+++ src/history.rb
@@ -29,6 +29,18 @@ class MainApp < Sinatra::Base
         json (@spending_access.history_all)
     end
 
+    # market_id で取得するエンドポイント
+    get '/history/market/:id' do
+        id = params[:id]
+        json (@spending_access.get_historys_by_market(id))
+    end
+
+    # type_id で取得するエンドポイント
+    get '/history/type/:id' do
+        id = params[:id]
+        json (@spending_access.get_historys_by_type(id))
+    end
+
     # 店情報を日付から取得するエンドポイント
     # このエンドポイントのjsonのparameterは、
     #   {
@@ -38,8 +50,18 @@ class MainApp < Sinatra::Base
     get '/history' do
         # HTTPリクエストのJSONのparameterをRubyで扱えるようにパースする
         # :keyがキーになる (e.g, params[:name])
-        params = JSON.parse(request.body.read, {:symbolize_names => true})
 
+        # なお、授業では堀り下げなかったがここでは例外処理を行っている
+        # なぜならjson文字列をruby#hashにうまくパースできなかったときにどうするか記述する必要があるからである
+        # begin内で失敗した場合、rescue以下が実行される
+        #
+        # 詳しくは https://docs.ruby-lang.org/ja/latest/doc/spec=2fcontrol.html#begin
+        begin
+            params = JSON.parse(request.body.read, {:symbolize_names => true})
+        rescue JSON::ParserError => e
+            status 400
+            return
+        end
 
         # getメソッドはクライアントが情報の塊であるjsonを取得したいので、`json (XXXX)` で返す
         data = @spending_access.get_history_by_period(params[:first], params[:last])
@@ -47,7 +69,6 @@ class MainApp < Sinatra::Base
         # データがnilの場合、クライアント由来のエラーなので4XXを返す(この場合400)
         if data == nil then
             status 400
-            json ({})
             return
         end
 
